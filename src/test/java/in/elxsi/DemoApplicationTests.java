7e6e2d4a62d21a2142c618c65ff74ef641c61dd2
scott <scott@gmail.com>
2025-10-25 21:56:01 +0530

    created simple test case

---
+++ src/test/java/in/elxsi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.elxsi;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DemoApplicationTests {
+
+    @Test
+    void sampleTest() {
+        int sum = 2 + 3;
+        assertEquals(5, sum, "2 + 3 should equal 5");
+    }
+}
